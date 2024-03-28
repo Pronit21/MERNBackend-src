570eace4bef4deae79ecb7ace880519c8215dea8
Pronit21 <pronit.parida.btech2020@sitpune.edu.in>
2024-03-28 11:22:23 +0530

    fixed bugs

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -21,7 +21,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required");
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username}, {email}]
     })
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller";
+import { registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router()
@@ -9,7 +9,7 @@ router.route("/register").post(
         {
             name: "avatar",
             maxCount: 1
-        },
+        }, 
         {
             name: "coverImage",
             maxCount: 1
